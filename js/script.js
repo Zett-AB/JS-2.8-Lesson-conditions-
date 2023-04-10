e261b607caf3244435e0ba6bb76f9fe91979beb7
Alexander <formysite.ru@yandex.ru>
2023-04-10 19:06:34 +0300

    not css

--- js/script.js
+++ js/script.js
@@ -15,10 +15,54 @@
 // }
 // alert(num); // 13;
 
-const num = 5;
+// const num = 5;
 
-if (num % 2) {
-    alert('Число нечётное!');
-    } else {
-    alert('Число чётное!');
-    }
+// if (num % 2) {
+//     alert('Число нечётное!');
+//     } else {
+//     alert('Число чётное!');
+//     }
+
+// const lang = 'ru';
+//     if (lang === 'ru') {
+//         alert('Это русский текст');
+//         } else if (lang === 'en') {
+//         alert('Это английский текст');
+//         } else if (lang !== 'ru' || lang !== 'en') {
+//         alert('Это не русский и не английский текст');
+//     }
+
+
+
+// function greeting(time) {
+//     if (time > 18) {
+//         return 'Добрый вечер!';
+//         } else if (time > 11) {
+//             return 'Добрый день!';
+//         } else if (time > 4) {
+//             return 'Доброе утро!';
+//         }
+//         return 'Доброй ночи!';
+//         }
+
+//     let grn=greeting(10); // "Доброе утро!"
+//     alert(grn);
+
+// const value = 10;
+// const result = value > 10 ? 'Число больше 10!' : 'Число равно или меньше 10!';
+// alert(result); // "Число равно или меньше 10!"
+  
+
+// const dayNumber = new Date().getDay();
+//     let day = 
+//         (dayNumber === 0) ? 'Воскресенье' :
+//             (dayNumber === 1) ? 'Понедельник' :
+//                 (dayNumber === 2) ? 'Вторник' :
+//                     (dayNumber === 3) ? 'Среда' :
+//                         (dayNumber === 4) ? 'Четверг' :
+//                             (dayNumber === 5) ? 'Пятница' :
+//                                 (dayNumber === 6) ? 'Суббота' : 'Неизвестный день недели';
+
+// alert(`Сегодня ${day.toLowerCase()}.`);
+
+
