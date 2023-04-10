fc37acdffdfd2cd12695159178bd42ab98b1fb7e
Alexander <formysite.ru@yandex.ru>
2023-04-10 16:32:02 +0300

    somewhere in the middle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-'use strict'
+'use strict';
+
+
+// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
+
+// if (year == 2015) {
+//     alert( "Правильно!" );
+//     alert( "Вы такой умный!" );
+//     }
+ 
+
+// let num = 8;
+// if (num > 4) {
+//   num += 5;
+// }
+// alert(num); // 13;
+
+const num = 5;
+
+if (num % 2) {
+    alert('Число нечётное!');
+    } else {
+    alert('Число чётное!');
+    }
